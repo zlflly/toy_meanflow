8216da5496172e125042284796a7baf48ec4ae6c
zlflly <1120241057@bit.edu.cn>
2026-07-28 15:36:56 +0800

    增加在码本基础上的decode，也就是把连续的embedding向量decode为token id，这个decode过程其实是最近邻搜索

--- toy_meanflow/codebook.py
+++ toy_meanflow/codebook.py
@@ -27,6 +27,7 @@ class FixedGaussianCodebook(nn.Module):
         )
         """创建码本，每一行对应一个token i的连续向量"""
 
+        self.embedding_dim = embedding_dim
         self.register_buffer("table", table) # buffer不会计算梯度，不会优化
 
     def encode(
@@ -35,3 +36,31 @@ class FixedGaussianCodebook(nn.Module):
     ) -> torch.Tensor:
         """会把每一个token id变成码本中对应的连续向量，这个token的维度从1变成embedding_dim"""
         return self.table[token_ids]
+
+    def decode(
+        self,
+        vectors: torch.Tensor,
+    ) -> torch.Tensor:
+        """使用最近邻搜索"""
+        if vectors.shape[-1] != self.embedding_dim:
+            raise ValueError("the last dimension must equal embedding_dim")
+
+        original_shape = vectors.shape[:-1] # 只需要去掉最后一个维度，就可以得到原来的维度
+        """[2, 8, embedding_dim]
+            original_shape = [2, 8]
+        """
+
+        flat_vectors = vectors.reshape(
+            -1, # -1表示让pytorch自己判断
+            self.embedding_dim,
+        ) # reshape，相当于把几个batch的待解码的向量拼在一起，最后一个维度是embedding_dim，第一个维度就是要解码的向量的个数
+
+        distances = torch.cdist( # torch.cdist() 会计算每个待解码向量与每个码本向量之间的欧氏距离
+            flat_vectors,
+            self.table,
+        )
+        '''输出的形状是[batch_size*seq_len, vocab_size]（分别是两个二维向量的第一个维度），也就是[i, j] 表示第i个连续向量，与第j个token表示的连续向量之间的欧式距离'''
+
+        token_ids = distances.argmin(dim=1) # 取最小值对应的tokne_id
+
+        return token_ids.reshape(original_shape)
